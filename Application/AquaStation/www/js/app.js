9824c15028cfef02886b52c9f091a0489fac9501
baboundu81 <baboundu81@gmail.com>
2016-05-02 19:37:00 +0200

    Tabs in the bottom even in android Android app in immersive fun screen mode Instant measure tab content centered (vertical and horizontal) PHP script OK to get data from data base to application HTTP request OK to get data from data base in the application

--- Application/AquaStation/www/js/app.js
+++ Application/AquaStation/www/js/app.js
@@ -1,9 +1,24 @@
+// Global var
+var monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
+  "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
+];
+
+// Personal functions
+function pad(num, size) {
+    var s = "000000000" + num;
+    return s.substr(s.length-size);
+}
+
+function dateToString(date) {
+    return((date.getDay() + 1) + ((date.getDay() + 1) == 1 ? 'er' : '') + ' ' + monthNames[date.getMonth()] + ' ' + date.getFullYear() + ' à ' + pad(date.getHours(), 2) + 'h' + pad(date.getMinutes(), 2));
+}
+
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'chart.js'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -61,17 +76,92 @@ angular.module('starter', ['ionic'])
     $urlRouterProvider.otherwise('/tab/instantpH')
 })
 
-.controller('instantpHCtrl', function($scope) {
-    $scope.test = 7.4;
-    console.log('instantpHCtrl');
+.config(function($ionicConfigProvider) {
+    $ionicConfigProvider.tabs.position('bottom');
 })
 
-.controller('instantTemperatureCtrl', function($scope) {
-    $scope.test = 12.5;
+.controller('instantpHCtrl', function($scope, $http) {
+    $http.get("http://lepetitvideaste.com/dataToApplication.php?mode=instant&dataType=pH")
+    .then(function (response) {
+        $scope.pH = response.data.records.Value;
+        var date = response.data.records.Date_time;
+        // Split timestamp into [ Y, M, D, h, m, s ]
+        var t = date.split(/[- :]/);
+
+        // Apply each element to the Date function
+        var d = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
+        $scope.date = dateToString(d);
+    });
+})
+
+.controller('instantTemperatureCtrl', function($scope, $http) {
+    $http.get("http://lepetitvideaste.com/dataToApplication.php?mode=instant&dataType=temperature#")
+    .then(function (response) {
+        $scope.temperature = response.data.records.Value;
+        var date = response.data.records.Date_time;
+        // Split timestamp into [ Y, M, D, h, m, s ]
+        var t = date.split(/[- :]/);
+
+        // Apply each element to the Date function
+        var d = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
+        $scope.date = dateToString(d);
+    });
     console.log('instantTemperatureCtrl');
 })
 
 .controller('graphicsCtrl', function($scope) {
-    $scope.test = "tadaaa";
+    var ctx = document.getElementById('graphicsChart');
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "rgba(75,192,192,1)",
+                borderColor: "rgba(75,192,192,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+            },
+            {
+                label: "My Second dataset",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "rgba(75,192,192,0)",
+                borderColor: "rgba(130,30,45,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [30, 48, 27, 32, 47, 90, 23],
+            }
+        ]
+    };
+    var graphicsChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+    });
+    
     console.log('graphicsCtrl');
 });
